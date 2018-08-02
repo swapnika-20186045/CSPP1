28a4623ed60c7355660ae9f9db377a1436617ec7
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-02 19:32:45 +0530

    " -> 5 of 5 passed. pylint: 9.09/10 "

--- CSPP1-Practice/p3/longest_substring.py
+++ CSPP1-Practice/p3/longest_substring.py
@@ -5,25 +5,25 @@ Created on 2-8-2018
 
 def main():
     '''longest alphabetical sequence in given string'''
-    s_1 = input()
-    s1 = ''
-    fs = ''
-    n = len(s_1)
+    st_r = input()
+    s_1 = ''
+    f_s = ''
+    n = len(st_r)
     f_max = 0
     f_cnt = 0
-    fs = s_1[0]
+    f_s = st_r[0]
     for i in range(0, n-1):
-        if ord(s_1[i]) <= ord(s_1[i+1]):
+        if ord(st_r[i]) <= ord(st_r[i+1]):
             f_cnt += 1
-            fs = fs + s_1[i+1]
+            f_s = f_s + st_r[i+1]
         else:
             if f_cnt > f_max:
                 f_max = f_cnt
-                s1 = fs
+                s_1 = f_s
             f_cnt = 0
-            fs = s_1[i+1]
+            f_s = st_r[i+1]
     if f_cnt > f_max:
-        s1 = fs
-    print(s1)
+        s_1 = f_s
+    print(s_1)
 if __name__== "__main__":
     main()
