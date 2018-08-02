414294c065cd8d9e589695f070d7673c48133696
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-02 17:25:51 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- CSPP1-Practice/sample-assignment/sol.py
+++ CSPP1-Practice/sample-assignment/sol.py
@@ -4,10 +4,12 @@ Created on 2-8-2018
 '''
 
 def main():
-	'''factorial'''
+	'''
+	factorial
+	'''
     data = input()
     print(int(data[0])+int(data[3]))
 
 if __name__ == "__main__":
     main()
-'''
+
