75d4fe4530bc13bde9fe2a80b129cb40c8e052bd
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-02 18:52:59 +0530

    " -> 3 of 5 passed. pylint: 10.00/10 "

---
+++ CSPP1-Practice/p2/bob_counter.py
@@ -0,0 +1,18 @@
+'''
+Author:Swapnika
+Created on 2-8-2018
+'''
+
+def main():
+    '''
+    number of times a small string occurs
+    '''
+    st_r = input()
+    coun_t = 0
+    for i in range(0, len(st_r)-3):
+        if (st_r[i] == 'b' and st_r[i+1] == 'o' and st_r[i+2] == 'b'):
+            coun_t += 1
+    print(coun_t)
+
+if __name__ == "__main__":
+    main()
