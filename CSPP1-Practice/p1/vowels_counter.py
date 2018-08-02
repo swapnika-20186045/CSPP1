bb429e7fd56816167cfdcd4025934286be312006
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-02 17:14:15 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

---
+++ CSPP1-Practice/p1/vowels_counter.py
@@ -0,0 +1,21 @@
+'''
+
+Author:Swapnika
+Created on 2-8-2018
+
+'''
+
+
+def main():
+    '''
+    function is used to count number of vowels
+    '''
+    st_r = input()
+    coun_t = 0
+    for cha_r in st_r:
+        if cha_r in ('a', 'e', 'i', 'o', 'u'):
+            coun_t += 1
+    print("vowels count is:" + str(coun_t))
+
+if __name__ == "__main__":
+    main()
--- CSPP1-Practice/sample-assignment/sol.py
+++ CSPP1-Practice/sample-assignment/sol.py
@@ -1,13 +1,13 @@
-# Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and
-# returns the factorial of given number.
+'''
+Author: Swapnika
+Created on 2-8-2018
+'''
 
-# This function takes in one number and returns one number.
-
-#using main function
 def main():
+	'''factorial'''
     data = input()
     print(int(data[0])+int(data[3]))
 
 if __name__ == "__main__":
     main()
+'''
