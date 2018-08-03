746c094952162abbef48af6c6cf1b5c98fe3f10e
swapnika-20186045 <41896273+swapnika-20186045@users.noreply.github.com>
2018-08-03 17:21:49 +0530

    Delete square_root_newtonrapson.py

--- CSPP1-Practice/M5/p4/square_root_newtonrapson.py
+++
@@ -1,9 +0,0 @@
-# Write a python program to find the square root of the number 
-# using Newton-Rapson method
-
-def main():
-	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
