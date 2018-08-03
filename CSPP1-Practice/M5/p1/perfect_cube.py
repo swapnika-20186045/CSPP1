7b7d133858f5447660437b4939b28633decd3cfa
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-03 14:59:09 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

---
+++ CSPP1-Practice/M5/p1/perfect_cube.py
@@ -0,0 +1,20 @@
+'''
+Author:Swapnika
+Created on 3-08-2018
+'''
+
+def main():
+    '''Write a python program to find if the given number is a perfect cube or not
+    using guess and check algorithm'''
+    print("enter an integer: ")
+    nu_m = input()
+    ans = 0
+    while ans ** 3 < int(nu_m):
+        ans = ans + 1
+    if ans ** 3 == int(nu_m):
+        print(str(nu_m) + 'is a perfect cube')
+    else:
+        print(str(nu_m) + 'is not a perfect cube')
+
+if __name__ == "__main__":
+    main()
