78b63e4cbbde5669fc57d52796e72cd9c1492445
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-03 17:56:36 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

---
+++ CSPP1-Practice/M5/p3/square_root_bisection.py
@@ -0,0 +1,27 @@
+'''
+Author:Swapnika
+Created on 3-08-2018
+'''
+
+def main():
+    '''# Write a python program to find the square root of the given number
+    using approximation method'''
+    sq_r = input()
+    epsil_on = 0.01
+    #s_p = 0.1
+    num_guesses = 0
+    lo_w = 0
+    hig_h = int(sq_r)
+    gu_ess = (hig_h + lo_w)/2.0
+    while abs(gu_ess**2 - int(sq_r)) >= epsil_on:
+        num_guesses += 1
+        if gu_ess**2 < int(sq_r):
+            lo_w = gu_ess
+        else:
+            hig_h = gu_ess
+        gu_ess = (hig_h + lo_w)/2.0
+    print('num_guesses=', num_guesses)
+    print(gu_ess, 'is close to the square root of', sq_r)
+
+if __name__ == "__main__":
+    main()
--- CSPP1-Practice/M5/p4/square_root_newtonrapson.py
+++ CSPP1-Practice/M5/p4/square_root_newtonrapson.py
@@ -1,9 +1,20 @@
-# Write a python program to find the square root of the number 
-# using Newton-Rapson method
+'''
+Author: Swapnika
+Created on 3-08-2018
+'''
 
 def main():
-	s = raw_input()
-	#your code here
+    '''Write a python program to find the square root of the number
+    using Newton-Rapson method'''
+    epsilon = 0.01
+    sq_r = input()
+    guess = int(sq_r)/2.0
+    num_guesses = 0
+    while abs(guess*guess - int(sq_r)) >= epsilon:
+        num_guesses += 1
+        guess = guess -(((guess**2) - int(sq_r))/(2*guess))
+    print('num_guesses = '+" "  +str(num_guesses))
+    print('Square root of' + str(sq_r) + ' is about ' + str(guess))
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
