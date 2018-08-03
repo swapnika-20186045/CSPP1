437a5c4e15de606ccc763146b34454909d13942e
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-03 16:50:04 +0530

    " -> 0 of 1 passed. pylint: 10.00/10 "

---
+++ CSPP1-Practice/M5/p2/square_root.py
@@ -0,0 +1,24 @@
+'''
+Author:Swapnika
+Created on 3-08-2018
+'''
+
+def main():
+    '''Write a python program to find the square root of the given number
+    using approximation method'''
+    sq_r = input()
+    epsilon = 0.01
+    guess = 0.0
+    increment = 0.0001
+    num_guesses = 0
+    while abs(guess**2 - int(sq_r)) >= epsilon:
+        guess += increment
+        num_guesses += 1
+    print('num_guesses=', num_guesses)
+    if abs(guess**2 - int(sq_r)) >= epsilon:
+        print('Failed on square root of', sq_r)
+    else:
+        print(guess, 'is close to the square root of', sq_r)
+
+if __name__ == "__main__":
+    main()
