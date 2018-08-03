7db8d4927db826dec034a8547fe39eb343982395
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-03 18:40:22 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- CSPP1-Practice/M5/p3/square_root_bisection.py
+++ CSPP1-Practice/M5/p3/square_root_bisection.py
@@ -6,22 +6,22 @@ Created on 3-08-2018
 def main():
     '''# Write a python program to find the square root of the given number
     using approximation method'''
-    sq_r = input()
+    sq_r = int(input())
     epsil_on = 0.01
     #s_p = 0.1
     num_guesses = 0
     lo_w = 0
-    hig_h = int(sq_r)
+    hig_h = sq_r
     gu_ess = (hig_h + lo_w)/2.0
-    while abs(gu_ess**2 - int(sq_r)) >= epsil_on:
+    while abs(gu_ess**2 - sq_r) >= epsil_on:
         num_guesses += 1
-        if gu_ess**2 < int(sq_r):
+        if gu_ess**2 < sq_r:
             lo_w = gu_ess
         else:
             hig_h = gu_ess
         gu_ess = (hig_h + lo_w)/2.0
     # print('num_guesses=', num_guesses)
-    print(num_guesses)
+    print(gu_ess)
     # print(gu_ess, 'is close to the square root of', sq_r)
 
 if __name__ == "__main__":
