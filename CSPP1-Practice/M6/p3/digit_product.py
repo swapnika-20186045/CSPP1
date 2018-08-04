a042340bcf97d9d082f29988d5dd2021ab696729
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-04 11:48:11 +0530

    " -> 2 of 7 passed. pylint: 9.38/10 "

--- CSPP1-Practice/M6/p3/digit_product.py
+++ CSPP1-Practice/M6/p3/digit_product.py
@@ -10,9 +10,16 @@ def main():
     '''
     nu_m = int(input())
     pr_o = 1
-    while nu_m > 0:
-        pr_o = pr_o * (nu_m)%10
+    c_nt = 0
+    k = nu_m
+    while nu_m != 0:
+        c_nt +=1
         nu_m = nu_m // 10
+    tem_p = k
+    while tem_p > 0:
+        re_s = tem_p % 10
+        pr_o = pr_o * (re_s * c_nt)
+        tem_p = tem_p // 10
     print(pr_o)
 if __name__ == "__main__":
     main()
