38da8e8d14db653b1e81067fecf74666dff30321
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-04 11:27:41 +0530

    " -> 3 of 7 passed. pylint: 8.89/10 "

---
+++ CSPP1-Practice/M6/p3/digit_product.py
@@ -0,0 +1,18 @@
+'''
+Author : Swapnika
+Created on 4-08-2018
+'''
+
+def main():
+    '''Given a  number int_input, find the product of all the digits example: 
+    input: 123
+    output: 6
+    '''
+    nu_m = int(input())
+    pr_o = 1
+    while nu_m > 0:
+        pr_o = pr_o * (nu_m)%10
+        nu_m = nu_m // 10
+    print(pr_o)
+if __name__ == "__main__":
+    main()
