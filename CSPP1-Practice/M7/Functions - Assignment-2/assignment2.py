98791d2615b93b6ab7c96fb2b75fcd7633cf447d
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-06 20:17:29 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- CSPP1-Practice/M7/Functions - Assignment-2/assignment2.py
+++ CSPP1-Practice/M7/Functions - Assignment-2/assignment2.py
@@ -73,7 +73,7 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print(paying_debt_off_in_a_year(data[0], data[1]))
-    
+    print(paying_debt_off_in_a_year(data[0], data[1])
+
 if __name__ == "__main__":
     main()
