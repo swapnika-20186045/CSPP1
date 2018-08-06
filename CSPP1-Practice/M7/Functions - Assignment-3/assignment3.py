52303bd2401e146db0fbe47555b8bd8b15b0e94a
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-06 20:57:28 +0530

    " -> 0 of 10 passed. pylint: 1.60/10 "

--- CSPP1-Practice/M7/Functions - Assignment-3/assignment3.py
+++ CSPP1-Practice/M7/Functions - Assignment-3/assignment3.py
@@ -62,7 +62,7 @@ def payingDebtOffInAYear(initial_balance, annual_interest_rate):
         else:
             break
     # mfp = int(mfp*100)/100.0
-    return "Lowest Payment: "+str(round(mfp,2))
+    return "Lowest Payment: "+str(mfp)
 
 def main():
     data = input()
