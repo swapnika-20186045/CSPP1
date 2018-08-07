debfc8cb838066e49e38345e9a2996260fab4851
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-07 19:01:39 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- CSPP1-Practice/M8/p1/assignment1.py
+++ CSPP1-Practice/M8/p1/assignment1.py
@@ -7,19 +7,19 @@ Created on 06-08-2018
 #and returns the factorial of given number.
 # This function takes in one number and returns one number.
 
-def fac_t(nu_m):
+def fact_orial(nu_m):
     '''
     n is positive Integer
     returns: a positive integer, the factorial of n.
     '''
     if nu_m in (1, 0):
         return 1
-    return nu_m*fac_t(nu_m-1)
+    return nu_m*fact_orial(nu_m-1)
 
 def main():
     '''main function'''
-    a_a = input()
-    print(fac_t(int(a_a)))
+    a_val = input()
+    print(fact_orial(int(a_val)))
 
 if __name__ == "__main__":
     main()
