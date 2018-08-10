2a324618685b2bd97c893e548bdafb5faf5e2fbb
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-10 17:30:55 +0530

    " -> 6 of 6 passed. pylint: 0.62/10 "

--- CSPP1-Practice/M11/p2/assignment2.py
+++ CSPP1-Practice/M11/p2/assignment2.py
@@ -3,10 +3,11 @@ Author: Swapnika
 Created on 10-08-2018
 '''
 #Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
+#Implement the updateHand function. Make sure this function has no side effects:
+#i.e., it must not mutate the hand passed in. Before pasting your function
+#definition here, be sure you've passed the appropriate tests in test_ps4a.py.
 
-
-def updateHand(hand, word):
+def updateHand(given_hand, given_word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
@@ -23,25 +24,20 @@ def updateHand(hand, word):
     returns: dictionary (string -> int)
     """
 
-    for i in word:
-        if i in hand:
-            hand[i] -= 1
-    return hand
-
-
-    
+    for wo_rd in given_word:
+        if wo_rd in given_hand:
+            given_hand[wo_rd] -= 1
+    return given_hand
 
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
-
-
-if __name__== "__main__":
-	main()
+    n=input()
+    adict={}
+    for i in range(int(n)):
+        data=input()
+        l=data.split()
+        adict[l[0]]=int(l[1])
+    data1=input()
+    print(updateHand(adict,data1))
+        
+if __name__ == "__main__":
+    main()
