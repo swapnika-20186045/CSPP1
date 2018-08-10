e5b6d0e21d9818844e754c250a3529ec1991d883
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-10 18:32:49 +0530

    " -> 5 of 5 passed. pylint: 6.67/10 "

--- CSPP1-Practice/M11/p4/assignment4.py
+++ CSPP1-Practice/M11/p4/assignment4.py
@@ -3,29 +3,31 @@ Author: Swapnika
 Created on 09-08-2018
 '''
 #Exercise: Assignment-4
-#We are now ready to begin writing the code that interacts with the player. We'll be implementing the playHand function. This function allows the user to play out a single hand. First, though, you'll need to implement the helper calculateHandlen function, which can be done in under five lines of code.
+#We are now ready to begin writing the code that interacts with the player.
+#We'll be implementing the playHand function. This function allows the user to
+#play out a single hand. First, though, you'll need to implement the helper
+#calculateHandlen function, which can be done in under five lines of code.
 
-
-def calculateHandlen(hand):
+def calculate_handlen(ha_nd):
     """ 
     Returns the length (number of letters) in the current hand.
     
     hand: dictionary (string int)
     returns: integer
     """
-    sum = 0
-    for letter in hand.values():
-        sum += letter
-    return sum
+    sum_val = 0
+    for letter_val in ha_nd.values():
+        sum_val += letter_val
+    return sum_val
 
 def main():
-	n = input()
-	adict = {}
-	for i in range(int(n)):
-		data = input()
-		l = data.split()
-		adict[l[0]] = int(l[1])
-	print(calculateHandlen(adict))
-		
+    num_n = input()
+    a_dict = {}
+    for i_i in range(int(num_n)):
+        da_ta = input()
+        l_1 = da_ta.split()
+        a_dict[l_1[0]] = int(l_1[1])
+    print(calculate_handlen(a_dict))
+        
 if __name__ == "__main__":
-	main()
+    main()
