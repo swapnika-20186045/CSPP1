e04b5cf4094975be12d0c45a68b37c0449bf0b97
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-11 11:41:09 +0530

    " -> 0 of 2 passed. pylint: -13.68/10 "

---
+++ CSPP1-Practice/M12/p1/create_social_network.py
@@ -0,0 +1,63 @@
+'''
+Author: Swapnika
+Created on 11-08-2018
+
+Assignment-1 Create Social Network
+'''
+
+def create_social_network(data):
+    '''
+        The data argument passed to the function is a string
+        It represents simple social network data
+        In this social network data there are people following other people
+
+        Here is an example social network data string:
+        John follows Bryant,Debra,Walter
+        Bryant follows Olive,Ollie,Freda,Mercedes
+        Mercedes follows Walter,Robin,Bryant
+        Olive follows John,Ollie
+
+        The string has multiple lines and each line represents one person
+        The first word of each line is the name of the person
+        The second word is follows that separates the person from the followers
+        After the second word is a list of people separated by ,
+
+        create_social_network function should split the string on lines
+        then extract the person and the followers by splitting each line
+        finally add the person and the followers to a dictionary and
+        return the dictionary
+
+        Caution: watch out for trailing spaces while splitting the string.
+        It may cause your test cases to fail although your output may be right
+
+        Error handling case:
+        Return a empty dictionary if the string format of the data is invalid
+        Empty dictionary is not None, it is a dictionary with no keys
+    '''
+    # data = input()
+    # n = int(input)
+    # for i in range(n):
+    #     data = data.split(" ")
+    #     i = i.split("follows")
+    # return i
+    dictionary = {}
+    for i in range(0, len(data),2):
+        if data[i] not in dictionary:
+            dictionary[data[i]] = dictionary[data[i+1]].split(',')
+    return dictionary
+
+def main():
+    '''
+        handling testcase input and printing output
+    '''
+    string = ''
+    lines = int(input())
+    for i in range(lines):
+        i += 1
+        string += input()
+        string += '\n'
+
+    print(create_social_network(string))
+
+if __name__ == "__main__":
+    main()
---
+++ CSPP1-Practice/M12/p2/social_network.py
@@ -0,0 +1,83 @@
+'''
+Author: Swapnika
+Created on 11-08-2018
+
+This is a continuation of the social network problem
+There are 3 functions below that have to be completed
+Note: PyLint score need not be 10/10 for this assignment. We expect 9.5/10
+'''
+
+def valid(network,arg1,arg2):
+    if username in network:
+        return True
+    return False
+
+def follow(network, arg1, arg2):
+    '''
+        3 arguments are passed to this function
+        network is a dictionary representing the social network
+        arg1 and arg2 are two people in the network
+        follow function is called when arg1 wants to follows arg2
+        so, this should result in adding arg2 to the followers list of arg1
+        update the network dictionary and return it
+    '''
+    if not valid(arg1) or not valid(arg2):
+        print("user not found!!")
+    else:
+        network(arg1).append(arg2)
+    print (arg1 + " is followed by " +arg2)
+
+def unfollow(network, arg1, arg2):
+    '''
+        3 arguments are passed to this function
+        network is a dictionary representing the social network
+        arg1 and arg2 are two people in the network
+        unfollow function is called when arg1 wants to stop following arg2
+        so, this should result in removing arg2 from the followers list of arg1
+        update the network dictionary and return it
+    '''
+    
+    if not valid(arg1) or not valid(arg2):
+        print("user not found!!")
+        if arg2 in network[arg1]:
+            network(arg1).remove(arg2)
+    return "removed"
+    return "arg2 not in arg1 friendlist"
+
+def delete_person(network, arg1):
+    '''
+        2 arguments are passed to this function
+        network is a dictionary representing the social network
+        arg1 is a person in the network
+        delete_person function is called when arg1 wants to exit from the network
+        so, this should result in deleting arg1 from network
+        also, before deleting arg1, remove arg1 from the everyone's followers list
+        update the network dictionary and return it
+    '''
+    if not valid(arg1) or not valid(arg2):
+        print("user not found!!")
+        if arg1 in network:
+            delete(network[arg1])
+    return "arg1 delete from everyone's friendlist"
+    
+def main():
+    '''
+        handling testcase input and printing output
+    '''
+    network = eval(input())
+    lines = int(input())
+    for i in range(lines):
+        i += 1
+        line = input()
+        output = line.split(" ")
+        if output[0] == "follow":
+            network = follow(network, output[1], output[2])
+        elif output[0] == "unfollow":
+            network = unfollow(network, output[1], output[2])
+        elif output[0] == "delete":
+            network = delete_person(network, output[1])
+
+    print(network)
+
+if __name__ == "__main__":
+    main()
