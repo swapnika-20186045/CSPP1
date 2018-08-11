15f95d2be558c65e19b761461447b18f3d2df560
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-11 12:48:55 +0530

    " -> 0 of 2 passed. pylint: 8.71/10 "

--- CSPP1-Practice/M12/p2/social_network.py
+++ CSPP1-Practice/M12/p2/social_network.py
@@ -59,7 +59,7 @@ def delete_person(network, arg1):
     # if not valid(arg1) or not valid(arg2):
     #     print("user not found!!")
     if arg1 in network:
-        del network[arg1]
+        del adict[arg1]
     return arg1 + ": " + str([])
     return "arg1 delete from everyone's friendlist"
 
