b0d5fab28a3411e031da173395e23817877dd872
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-17 15:55:38 +0530

    " -> 0 of 3 passed. pylint: 6.52/10 "

--- CSPP1-Practice/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/CodeCampDocumentDistance/document_distance.py
@@ -1,12 +1,79 @@
 '''
     Document Distance - A detailed description is given in the PDF
 '''
+import re
 
-def similarity(dict1, dict2):
+def similarity(common_dict, dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    pass
+    num_val = 0
+    den1 = 0
+    den2 = 0
+    for i in common_dict:
+        num_val += common_dict[0] * common_dict[1]
+    for j in dict1:
+        den1 += dict1[j] ** 2
+    for k in dict2:
+        den2 += dict2[k] ** 2
+
+    return (num_val) / math.sqrt(den1*den2)
+
+def word_list(input1, input2):
+    list_1 = []
+    list_2 = []
+    key_list = []
+    a = ""
+    b = ""
+    
+    a = re.sub('[^A-Za-z0-9]',' ', input1.lower())
+    b = re.sub('[^A-Za-z0-9]',' ', input2.lower())
+    
+    list_1 = a.split(" ")
+    list_2 = b.split(" ")
+
+    stopwords = load_stopwords("stopwords.txt")
+    key_list = stopwords.keys()
+
+    for i in key_list:
+        for j in list_1:
+            if i == j:
+                list_1.remove(j)
+
+    for i in key_list:
+        for j in list_2:
+            if i == j:
+                list_2.remove(j)
+    freq_count(list_1, list_2)
+
+    # return list_1,list_2
+
+def freq_count(list_1, list_2):
+    freq_dict1 = {}
+    freq_dict2 = {}
+    common_dict = {}
+    for k in list_1:
+        if k not in freq_dict1:
+            freq_dict1[k] = 1
+        else:
+            freq_dict1[k] += 1
+
+    for k in list_2:
+        if k not in freq_dict2:
+            freq_dict2[k] = 1
+        else:
+            freq_dict2[k] += 1
+    # print(freq_dict1, freq_dict2)
+
+    for i in freq_dict1:
+        if i in freq_dict2:
+            common_dict[i] = [freq_dict1[i], freq_dict2[i]]
+        else:
+            common_dict[i] = [freq_dict1[i], 0]
+    for p in common_dict:
+        if p not in common_dict:
+            common_dict[p] = [0, freq_dict2[p]]
+    print(common_dict)
 
 def load_stopwords(filename):
     '''
@@ -16,7 +83,6 @@ def load_stopwords(filename):
     with open(filename, 'r') as filename:
         for line in filename:
             stopwords[line.strip()] = 0
-            print (stopwords)
     return stopwords
 
 def main():
@@ -26,7 +92,8 @@ def main():
     input1 = input()
     input2 = input()
 
-    print(similarity(input1, input2))
+    (common_dict, dict1, dict2) = word_list(input1, input2)
+    print(similarity(common_dict, dict1, dict2))
 
 if __name__ == '__main__':
     main()
