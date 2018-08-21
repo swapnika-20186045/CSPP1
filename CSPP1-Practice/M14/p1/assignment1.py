bd9904b35374469c64486a5302e926dbdf5a8d9a
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-21 21:14:13 +0530

    " -> 0 of 5 passed. pylint: 0.70/10 "

--- CSPP1-Practice/M14/p1/assignment1.py
+++ CSPP1-Practice/M14/p1/assignment1.py
@@ -65,15 +65,100 @@ WORDLIST_FILENAME = 'words.txt'
 
 
 ### Paste your implementation of the Message class here
-        
+class Message(object):
+    '''### DO NOT MODIFY THIS METHOD ###'''
+    def __init__(self, text):
+        '''
+        Initializes a Message object
+
+        text (string): the message's text
+
+        a Message object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words
+        '''
+        self.message_text = text
+        self.valid_words = load_words(WORDLIST_FILENAME)
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_message_text(self):
+        '''
+        Used to safely access self.message_text outside of the class
+
+        Returns: self.message_text
+        '''
+        return self.message_text
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_valid_words(self):
+        '''
+        Used to safely access a copy of self.valid_words outside of the class
+
+        Returns: a COPY of self.valid_words
+        '''
+        return self.valid_words[:]
+
+    def build_shift_dict(self, shift):
+        '''
+        Creates a dictionary that can be used to apply a cipher to a letter.
+        The dictionary maps every uppercase and lowercase letter to a
+        character shifted down the alphabet by the input shift. The dictionary
+        should have 52 keys of all the uppercase letters and all the lowercase
+        letters only.
+
+        shift (integer): the amount by which to shift every letter of the
+        alphabet. 0 <= shift < 26
+
+        Returns: a dictionary mapping a letter (string) to
+                 another letter (string).
+        '''
+        shift_dict = {}
+
+        while i <= 255:
+            if 65<=i<=91-shift:
+                d[chr(i)] = chr(i+shift)
+            elif 91-shift<=i<=91:
+                d[chr(i)] = chr(i+shift-26)
+            elif 97<=i<=123-shift:
+                d[chr(i)] = chr(i+shift)
+            elif 123-shift<=i<=123:
+                d[chr(i)] = chr(i+shift-26)
+            else:
+                d[chr(i)] = chr(i)
+            i += 1
+
+        return shift_dict
+        # pass #delete this line and replace with your code here
+
+    def apply_shift(self, shift):
+        '''
+        Applies the Caesar Cipher to self.message_text with the input shift.
+        Creates a new string that is self.message_text shifted down the
+        alphabet by some number of characters determined by the input shift
+
+        shift (integer): the shift with which to encrypt the message.
+        0 <= shift < 26
+
+        Returns: the message text (string) in which every character is shifted
+             down the alphabet by the input shift
+        '''
+        # pass #delete this line and replace with your code here
+        message = ""
+        shift_dict = self.build_shift_dict(shift)
+        for i in self.message_text:
+            message += shift_dict[i]
+        return message
 
 def main():
     '''
         Function to handle testcases
     '''
-    data = Message(input())
+    data = input()
+    shift = int(input())
+    # print(data, shift)
+    data = Message(data)
     data.get_message_text()
-    print(data.apply_shift(int(input())))
+    print(data.apply_shift(shift))
 
 if __name__ == "__main__":
     main()
