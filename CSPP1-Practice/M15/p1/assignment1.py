f66ccb70480bd8209cec4b781ae10a8c4c7e73a3
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-22 21:47:37 +0530

    " -> 0 of 3 passed. pylint: 8.67/10 "

--- CSPP1-Practice/M15/p1/assignment1.py
+++ CSPP1-Practice/M15/p1/assignment1.py
@@ -258,7 +258,7 @@ class CiphertextMessage(Message):
         decrypted_messages = []
         for each_shift in range(27):
             decrypted_messages.append(self.apply_shift(1))
-        return (decrypted_messages.index(max(decrypted_messages, key = self.no_of_valid_words))+1,\
+        return (26 - decrypted_messages.index(max(decrypted_messages, key = self.no_of_valid_words))+1,\
         	   max(decrypted_messages, key = self.no_of_valid_words))
 
 
