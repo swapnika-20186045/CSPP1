0da8741bc1caa2268613475bcdffb68041154e04
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-23 12:56:20 +0530

    " -> 8 of 8 passed. pylint: 6.23/10 "

--- CSPP1-Practice/CodeCampMatrixOperations/matrix_operations.py
+++ CSPP1-Practice/CodeCampMatrixOperations/matrix_operations.py
@@ -1,4 +1,4 @@
-def mult_matrix(m1, m2, n, m):
+def mult_matrix(m1, m2, n_1, m_1, n_2, m_2):
     '''
         check if the matrix1 columns = matrix2 rows
         mult the matrices and return the result matrix
@@ -6,19 +6,21 @@ def mult_matrix(m1, m2, n, m):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
+    if m_1 == n_2:
+        mul = []
+        for i in range(0, n_1):
+            mat = []
+            for j in range(0, n_1):
+                sum_val = 0
+                for n_2 in range(0, m_1):
+                    sum_val = sum_val + (m1[i][n_2] * m2[n_2][j])
+                mat.append(sum_val)
+            mul.append(mat)
+        return mul
+    else:
+        print("Error: Matrix shapes invalid for mult")
 
-    mul = []
-    for i in range(0, n):
-        mat = []
-        for j in range(0, n):
-            sum_val = 0
-            for k in range(0, m):
-                sum_val = sum_val + (m1[i][k] * m2[k][j])
-            mat.append(sum_val)
-        mul.append(mat)
-    return mul
-
-def add_matrix(m1, m2, n, m):
+def add_matrix(m1, m2, n_1, m_1, n_2, m_2):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -26,13 +28,16 @@ def add_matrix(m1, m2, n, m):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    add = []
-    for i in range(0, n):
-        mat = []
-        for j in range(0, m):
-            mat.append(m1[i][j] + m2[i][j])
-        add.append(mat)
-    return add
+    if n_1 == n_2 and m_1 == m_2:
+        add = []
+        for i in range(0, n_1):
+            mat = []
+            for j in range(0, m_1):
+                mat.append(m1[i][j] + m2[i][j])
+            add.append(mat)
+        return add
+    else:
+        print("Error: Matrix shapes invalid for addition")
 
 def read_matrix():
     '''
@@ -42,40 +47,47 @@ def read_matrix():
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
     '''
-    n,m = input().split(',')
-    m = int(m)
-    n = int(n)
+    n_1,m_1 = input().split(',')
+    m_1 = int(m_1)
+    n_1 = int(n_1)
     matrix1 = []
     matrix2 = []
-    for i in range(0, n):
+    for i in range(0, n_1):
         matrix1.append(list(map(int,input().split())))
     # print(matrix1)
-    n,m = input().split(',')
-    n = int(n)
-    for i in range(0, n):
-        matrix2.append(list(map(int,input().split())))
+    # n,m = input().split(',')
+    # n = int(n)
+    # for i in range(0, n):
+    #     matrix2.append(list(map(int,input().split())))
 
     flag = True
     for i in matrix1:
         count = 0
         for j in i:
             count += 1
-        if count != m:
+        if count != m_1:
             flag = False
     if flag == False:
         print("Error: Invalid input for the matrix")
+
+    n_2,m_2 = input().split(',')
+    m_2 = int(m_2)
+    n_2 = int(n_2)
+    for i in range(0, n_2):
+        matrix2.append(list(map(int,input().split())))
+
     flag = True
     for i in matrix2:
         count = 0
         for j in i:
             count += 1
-        if count != m:
+        if count != m_2:
             flag = False
     if flag == False:
         print("Error: Invalid input for the matrix")
     if flag == True:
-        print(add_matrix(matrix1, matrix2, n, m))
-        print(mult_matrix(matrix1, matrix2, n, m))
+        print(add_matrix(matrix1, matrix2, n_1, m_1, n_2, m_2))
+        print(mult_matrix(matrix1, matrix2, n_1, m_1, n_2, m_2))
 
 def main():
     # read matrix 1
