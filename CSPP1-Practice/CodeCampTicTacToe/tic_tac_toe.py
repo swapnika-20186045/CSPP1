3549f322e198d0485eed578f989156b8881bdfef
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-24 11:55:53 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- CSPP1-Practice/CodeCampTicTacToe/tic_tac_toe.py
+++ CSPP1-Practice/CodeCampTicTacToe/tic_tac_toe.py
@@ -1,9 +1,9 @@
 def is_horizontal(main_list, turn):
-	count = 0
-	for lo_op in range(3):
-		for in_loop in range(3):
+    count = 0
+    for lo_op in range(3):
+        for in_loop in range(3):
             if turn not in main_list[lo_op][in_loop]:
-            	count += 1
+                count += 1
         if count == 0:
             return True
         count = 0
@@ -12,23 +12,23 @@ def is_horizontal(main_list, turn):
 def is_vertical(main_list, turn):
     count = 0
     for in_loop in range(3):
-    	for lo_op in range(3):
-    		if turn not in main_list[in_loop][lo_op]:
-    			count += 1
-    	if count == 0:
+        for lo_op in range(3):
+            if turn not in main_list[in_loop][lo_op]:
+                count += 1
+        if count == 0:
             return True
         count = 0
     return False
 
 def is_diagonal_forward(main_list, turn):
-	count = 0
-	for lo_op in range(3):
-		if turn not in main_list[lo_op][lo_op]:
-			count += 1
-	if count == 0:
+    count = 0
+    for lo_op in range(3):
+        if turn not in main_list[lo_op][lo_op]:
+            count += 1
+    if count == 0:
             return True
         count = 0
-    return False			    	
+    return False                    
 
 def is_diagonal_backward(main_list, turn):
     '''Checks for diagnol backward match'''
@@ -51,20 +51,20 @@ def main():
     other_char = 0
     main_list = []
     for i in range(3):
-    	main_list.append(list(map(str, input().split())))
-    
+        main_list.append(list(map(str, input().split())))
+
     turn_x = 'x'
     boolean_x = (is_horizontal(main_list, turn_x)
                  or is_vertical(main_list, turn_x)
                  or is_diagonal_forward(main_list, turn_x)
                  or is_diagonal_backward(main_list, turn_x))
-    
+
     turn_o = 'o'
     boolean_o = (is_horizontal(main_list, turn_o)
                  or is_vertical(main_list, turn_o)
                  or is_diagnol_forward(main_list, turn_o)
                  or is_diagnol_backward(main_list, turn_o))
-    
+
     if boolean_x and boolean_o and count == 0:
         print("invalid game")
         count += 1
@@ -76,6 +76,6 @@ def main():
         count += 1
     if count == 0:
         print("draw")
-    
+
 if __name__ == '__main__':
     main()
