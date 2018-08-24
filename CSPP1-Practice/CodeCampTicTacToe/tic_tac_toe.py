4bc533a556bf204689cdcd95cc8875ca4505324f
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-24 11:55:18 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- CSPP1-Practice/CodeCampTicTacToe/tic_tac_toe.py
+++ CSPP1-Practice/CodeCampTicTacToe/tic_tac_toe.py
@@ -1,33 +1,81 @@
-def tic_tac_toe():
-    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    end = False
-    win_commbinations = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+def is_horizontal(main_list, turn):
+	count = 0
+	for lo_op in range(3):
+		for in_loop in range(3):
+            if turn not in main_list[lo_op][in_loop]:
+            	count += 1
+        if count == 0:
+            return True
+        count = 0
+    return False
 
-    def draw():
-        print(board[0], board[1], board[2])
-        print(board[3], board[4], board[5])
-        print(board[6], board[7], board[8])
-        print()
+def is_vertical(main_list, turn):
+    count = 0
+    for in_loop in range(3):
+    	for lo_op in range(3):
+    		if turn not in main_list[in_loop][lo_op]:
+    			count += 1
+    	if count == 0:
+            return True
+        count = 0
+    return False
 
-    def p1():
-        n = choose_number()
-        if board[n] == "X" or board[n] == "O":
-            print("\nYou can't go there. Try again")
-            p1()
-        else:
-            board[n] = "X"
+def is_diagonal_forward(main_list, turn):
+	count = 0
+	for lo_op in range(3):
+		if turn not in main_list[lo_op][lo_op]:
+			count += 1
+	if count == 0:
+            return True
+        count = 0
+    return False			    	
 
-    def p2():
-        n = choose_number()
-        if board[n] == "X" or board[n] == "O":
-            print("\nYou can't go there. Try again")
-            p2()
-        else:
-            board[n] = "O"
+def is_diagonal_backward(main_list, turn):
+    '''Checks for diagnol backward match'''
+    count = 0
+    in_loop = 2
+    for lo_op in range(3):
+        if turn not in main_list[lo_op][in_loop]:
+            count += 1
+        in_loop -= 1
+    if count == 0:
+        return True
+    return False
 
 def main():
     '''main function'''
-    tic_tac_toe()
-
+    count = 0
+    x_count = 0
+    o_count = 0
+    char_count = 0
+    other_char = 0
+    main_list = []
+    for i in range(3):
+    	main_list.append(list(map(str, input().split())))
+    
+    turn_x = 'x'
+    boolean_x = (is_horizontal(main_list, turn_x)
+                 or is_vertical(main_list, turn_x)
+                 or is_diagonal_forward(main_list, turn_x)
+                 or is_diagonal_backward(main_list, turn_x))
+    
+    turn_o = 'o'
+    boolean_o = (is_horizontal(main_list, turn_o)
+                 or is_vertical(main_list, turn_o)
+                 or is_diagnol_forward(main_list, turn_o)
+                 or is_diagnol_backward(main_list, turn_o))
+    
+    if boolean_x and boolean_o and count == 0:
+        print("invalid game")
+        count += 1
+    if boolean_x and count == 0:
+        print(turn_x)
+        count += 1
+    if boolean_o and count == 0:
+        print(turn_o)
+        count += 1
+    if count == 0:
+        print("draw")
+    
 if __name__ == '__main__':
     main()
