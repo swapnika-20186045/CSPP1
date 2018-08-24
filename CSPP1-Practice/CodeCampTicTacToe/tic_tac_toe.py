d8696c0e3d855e9f3e7f02458e398ab24269f2d2
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-24 12:28:04 +0530

    " -> 14 of 14 passed. pylint: 9.37/10 "

--- CSPP1-Practice/CodeCampTicTacToe/tic_tac_toe.py
+++ CSPP1-Practice/CodeCampTicTacToe/tic_tac_toe.py
@@ -60,7 +60,8 @@ def main():
                 o_count += 1
             elif main_list[lo_op][in_loop] == '.':
                 char_count += 1
-            other_char += 1
+            else:
+                other_char += 1
     if other_char != 0:
         print("invalid input")
         count += 1
