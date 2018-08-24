770272c410d09008cce938da177e3b3a4c13bffc
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-24 10:43:03 +0530

    " -> 0 of 14 passed. pylint: 2.00/10 "

---
+++ CSPP1-Practice/CodeCampTicTacToe/tic_tac_toe.py
@@ -0,0 +1,33 @@
+def tic_tac_toe():
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end = False
+    win_commbinations = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+
+    def draw():
+        print(board[0], board[1], board[2])
+        print(board[3], board[4], board[5])
+        print(board[6], board[7], board[8])
+        print()
+
+    def p1():
+        n = choose_number()
+        if board[n] == "X" or board[n] == "O":
+            print("\nYou can't go there. Try again")
+            p1()
+        else:
+            board[n] = "X"
+
+    def p2():
+        n = choose_number()
+        if board[n] == "X" or board[n] == "O":
+            print("\nYou can't go there. Try again")
+            p2()
+        else:
+            board[n] = "O"
+
+def main():
+    '''main function'''
+    tic_tac_toe()
+
+if __name__ == '__main__':
+    main()
