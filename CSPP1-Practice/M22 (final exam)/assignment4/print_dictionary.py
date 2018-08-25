a0ff98b1e8e2a86a97fc63bb2ea3e7e776af2a4b
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 12:13:56 +0530

    " -> 0 of 3 passed. pylint: -5.56/10 "

---
+++ CSPP1-Practice/M22 (final exam)/assignment4/print_dictionary.py
@@ -0,0 +1,20 @@
+'''
+Write a function to print a dictionary with the keys in sorted order along with the
+frequency of each word. Format of the printing should be one key per line and separate
+the key and frequency with a SPACE - SPACE.
+
+Author: Swapnika
+Date: 25-08-2018
+'''
+
+def print_dictionary(dictionary):
+    keys = sorted(index.keys())
+    for key in keys:
+        print(key, " - ", index[key])
+
+def main():
+    dictionary = eval(input())
+    print_dictionary(dictionary)
+
+if __name__ == '__main__':
+    main()
