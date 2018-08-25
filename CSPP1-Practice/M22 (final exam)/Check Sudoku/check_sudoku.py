6c1400b5cfb90c5ef072882b2493b23426e97bf3
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 12:54:25 +0530

    " -> 0 of 26 passed. pylint: -6.04/10 "

---
+++ CSPP1-Practice/M22 (final exam)/Check Sudoku/check_sudoku.py
@@ -0,0 +1,79 @@
+'''
+    Sudoku is a logic-based, combinatorial number-placement puzzle.
+    The objective is to fill a 9×9 grid with digits so that
+    each column, each row, and each of the nine 3×3 subgrids that compose the grid
+    contains all of the digits from 1 to 9.
+
+    Complete the check_sudoku function to check if the given grid
+    satisfies all the sudoku rules given in the statement above.
+
+Author: Swapnika
+Date: 25-08-2018
+'''
+def is_box(sudoku):
+    for i in range(9):
+        for j in range(9):
+            if not '9' >= sudoku[i][j] >= '0':
+                return False
+
+def is_horizontal(sudoku):
+    for i in sudoku:
+        if "".join(sorted(i)) !=  line:
+            return False
+
+def is_vertical(sudoku):
+    for i in range(9):
+        x = []
+        for j in range(9):
+            x.append(sudoku[j][i])
+        if "".join(sorted(x)) != line:
+            return False
+
+def check_sudoku(sudoku):
+    '''
+        Your solution goes here. You may add other helper functions as needed.
+        The function has to return True for a valid sudoku grid and false otherwise
+    '''
+    if "".join(sorted(sudoku[0][0]+sudoku[0][1]+sudoku[0][2]+sudoku[1][0]+sudoku[1][1]+sudoku[1][2]+sudoku[2][0]+sudoku[2][1]+sudoku[2][2])) != line:
+        return False
+    if "".join(sorted(sudoku[0][3]+sudoku[0][4]+sudoku[0][5]+sudoku[1][3]+sudoku[1][4]+sudoku[1][5]+sudoku[2][3]+sudoku[2][4]+sudoku[2][5])) != line:
+        return False
+    if "".join(sorted(sudoku[0][6]+sudoku[0][7]+sudoku[0][8]+sudoku[1][6]+sudoku[1][7]+sudoku[1][8]+sudoku[2][6]+sudoku[2][7]+sudoku[2][8])) != line:
+        return False
+    if "".join(sorted(sudoku[3][0]+sudoku[3][1]+sudoku[3][2]+sudoku[4][0]+sudoku[4][1]+sudoku[4][2]+sudoku[5][0]+sudoku[5][1]+sudoku[5][2])) != line:
+        return False
+    if "".join(sorted(sudoku[3][3]+sudoku[3][4]+sudoku[3][5]+sudoku[4][3]+sudoku[4][4]+sudoku[4][5]+sudoku[5][3]+sudoku[5][4]+sudoku[5][5])) != line:
+        return False
+    if "".join(sorted(sudoku[3][6]+sudoku[3][7]+sudoku[3][8]+sudoku[4][6]+sudoku[4][7]+sudoku[4][8]+sudoku[5][6]+sudoku[5][7]+sudoku[5][8])) != line:
+        return False
+    if "".join(sorted(sudoku[6][0]+sudoku[6][1]+sudoku[6][2]+sudoku[7][0]+sudoku[7][1]+sudoku[7][2]+sudoku[8][0]+sudoku[8][1]+sudoku[8][2])) != line:
+        return False
+    if "".join(sorted(sudoku[6][3]+sudoku[6][4]+sudoku[6][5]+sudoku[7][3]+sudoku[7][4]+sudoku[7][5]+sudoku[8][3]+sudoku[8][4]+sudoku[8][5])) != line:
+        return False
+    if "".join(sorted(sudoku[6][6]+sudoku[6][7]+sudoku[6][8]+sudoku[7][6]+sudoku[7][7]+sudoku[7][8]+sudoku[8][6]+sudoku[8][7]+sudoku[8][8])) != line:
+        return False
+    return True
+
+
+def main():
+    '''
+        main function to read input sudoku from console
+        call check_sudoku function and print the result to console
+    '''
+    
+    # initialize empty list
+    sudoku = []
+    line = "123456789"
+    # loop to read 9 lines of input from console
+    for i in range(9):
+        # read a line, split it on SPACE and append row to list
+        row = input().split(' ')
+        sudoku.append(row)
+    # call solution function and print result to console
+    is_box(sudoku)
+    is_horizontal(sudoku)
+    is_vertical(sudoku)
+    print(check_sudoku(sudoku))
+
+if __name__ == '__main__':
+    main()
--- CSPP1-Practice/M22 (final exam)/assignment5/frequency_graph.py
+++ CSPP1-Practice/M22 (final exam)/assignment5/frequency_graph.py
@@ -1,6 +1,9 @@
 '''
 Write a function to print a dictionary with the keys in sorted order along with the
 frequency of each word. Display the frequency values using “#” as a text based graph
+
+Author: Swapnika
+Date: 25-08-2018
 '''
 
 def frequency_graph(dictionary):
