99b7c4fe37d23ca5e79a15134d17189f4572cf49
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 10:29:14 +0530

    " -> 0 of 2 passed. pylint: 0/0 "

---
+++ CSPP1-Practice/M22 (final exam)/assignment1/read_input.py
@@ -0,0 +1,16 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def read_input():
+	list_1 = []
+
+    for _ in range(n):
+        list_1.append(input())
+    return list_1
+
+def main():
+    read_input()
+
+if __name__ == '__main__':
+    main()
