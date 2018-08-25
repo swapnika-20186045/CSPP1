6a8afa6cd2d4b54490a7c18a3b4bc786b9f5d208
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 11:51:25 +0530

    " -> 0 of 4 passed. pylint: -3.33/10 "

--- CSPP1-Practice/M22 (final exam)/assignment3/tokenize.py
+++ CSPP1-Practice/M22 (final exam)/assignment3/tokenize.py
@@ -20,13 +20,20 @@ Date: 25-08-2018
 #         freq[x] = freq.get(x, 0) + 1
 #     return freq
 
-def main():
-    '''main function'''
-    num_n = input()
-    a_dict = {}
+def clean_input(da_ta):
     for i in range(int(num_n)):
         da_ta = input()
         l_l = da_ta.split()
         i += 1
+    return l_l
+
+def main():
+    '''main function'''
+    num_n = input()
+    # a_dict = {}
+    # for i in range(int(num_n)):
+    #     da_ta = input()
+    #     l_l = da_ta.split()
+    #     i += 1
     # data_1 = input()
-    print(l_l)
+    print(clean_input(da_ta))
