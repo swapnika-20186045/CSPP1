77249d215c6cce7db9e9475f749063b54af56c38
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 10:30:12 +0530

    " -> 0 of 2 passed. pylint: 7.00/10 "

--- CSPP1-Practice/M22 (final exam)/assignment1/read_input.py
+++ CSPP1-Practice/M22 (final exam)/assignment1/read_input.py
@@ -3,7 +3,8 @@ Write a python program to read multiple lines of text input and store the input 
 '''
 
 def read_input():
-	list_1 = []
+    list_1 = []
+    n = len(input)
 
     for _ in range(n):
         list_1.append(input())
