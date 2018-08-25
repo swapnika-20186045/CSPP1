d278ba72370400e9432fe3863f66aec04e9b9970
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 11:20:05 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

---
+++ CSPP1-Practice/M22 (final exam)/assignment3/tokenize.py
@@ -0,0 +1,20 @@
+'''
+Write a function to tokenize a given string and return a dictionary with the frequency of
+each word
+
+Author: Swapnika
+Date: 25-08-2018
+'''
+
+def tokenize(string):
+	string = input()
+	str_1 = ""
+	for i in string:
+		str_1 = input().split()
+    return str_1
+            
+def main():
+    print(tokenize(string))
+
+if __name__ == '__main__':
+    main()
