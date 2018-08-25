7504fb3a4afba9fb26ebc6b6ae833a5df1fc2f11
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 11:33:48 +0530

    " -> 0 of 4 passed. pylint: 0.83/10 "

--- CSPP1-Practice/M22 (final exam)/assignment3/tokenize.py
+++ CSPP1-Practice/M22 (final exam)/assignment3/tokenize.py
@@ -11,15 +11,15 @@ def tokenize(string):
     
     
     for i in string:
-        # str_1 = re.sub('[^ A-Za-z0-9]', '', string.lower())
-        str_1 = input().split()
+        str_1 = re.sub('[^ A-Za-z0-9]', '', string.lower())
+        # str_1 = input().split()
     return str_1
 
-def getFrequencyDict(str_1):
-    freq = {}
-    for x in str_1:
-        freq[x] = freq.get(x, 0) + 1
-    return freq
+# def getFrequencyDict(str_1):
+#     freq = {}
+#     for x in str_1:
+#         freq[x] = freq.get(x, 0) + 1
+#     return freq
       
 def main():
     str_1 = ""
