ecd435f9e890fc65e4972dfd95673e285c6023f8
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 12:08:09 +0530

    " -> 0 of 4 passed. pylint: 2.22/10 "

--- CSPP1-Practice/M22 (final exam)/assignment3/tokenize.py
+++ CSPP1-Practice/M22 (final exam)/assignment3/tokenize.py
@@ -5,32 +5,52 @@ each word
 Author: Swapnika
 Date: 25-08-2018
 '''
-import re
+# import re
 def tokenize(string):
-    wordlist = string.split()
-    wordfreq = []
-    for i in wordlist:
-        wordfreq.append(wordlist.count(i))
-    return wordfreq
-    # for i in string:
-    #     str_1 = input().split()
-    # return str_1
-def wordListToFreqDict(wordlist):
-    wordfreq = [wordlist.count(p) for p in wordlist]
-    return dict(zip(wordlist,wordfreq))
+    a_dict = {}
+    # initialize a search index (an empty dictionary)
+    doc_list = string
+    len_doc_list = len(doc_list)
+    for i in range(len_doc_list):
+        doc_list[i] = word_list(doc_list[i])
+        doc_list[i] = collections.Counter(doc_list[i])
+    # iterate through all the string
+    # keep track of doc_id which is the list index corresponding the document
+    # hint: use enumerate to obtain the list index in the for loop
+    for doc_id, doc in enumerate(doc_list):
+        for word in doc:
+            if word in a_dict:
+                a_dict[word].append((doc_id, doc_list[doc_id][word]))
+            else:
+                a_dict[word] = [(doc_id, doc_list[doc_id][word])]
+        # clean up doc and tokenize to words list
+        # add or update the words of the doc to the search index
+    return a_dict
+
+#     wordlist = string.split()
+#     wordfreq = []
+#     for i in wordlist:
+#         wordfreq.append(wordlist.count(i))
+#     return wordfreq
+#     # for i in string:
+#     #     str_1 = input().split()
+#     # return str_1
+# def wordListToFreqDict(wordlist):
+#     wordfreq = [wordlist.count(p) for p in wordlist]
+#     return dict(zip(wordlist,wordfreq))
 
 
-# def getFrequencyDict(sequence):
-#     freq = {}
-#     for x in sequence:
-#         freq[x] = freq.get(x, 0) + 1
-#     return freq
+# # def getFrequencyDict(sequence):
+# #     freq = {}
+# #     for x in sequence:
+# #         freq[x] = freq.get(x, 0) + 1
+# #     return freq
       
 def main():
-    wordlist = []
+    # wordlist = []
     string = input()
     print(tokenize(string))
-    print(wordListToFreqDict(wordlist))
+    # print(wordListToFreqDict(wordlist))
     # getFrequencyDict(str_1)
 
 if __name__ == '__main__':
