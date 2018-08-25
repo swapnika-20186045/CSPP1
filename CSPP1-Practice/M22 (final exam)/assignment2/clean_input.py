66cee7442a7d996dc3fc19ce697a3579622d3835
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 10:48:35 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

---
+++ CSPP1-Practice/M22 (final exam)/assignment2/clean_input.py
@@ -0,0 +1,19 @@
+'''
+Write a function to clean up a given string by removing the special characters and retain 
+alphabets in both upper and lower case and numbers.
+
+Author: Swapnika
+Date: 25-08-2018
+'''
+
+def clean_string(string):
+	str_1 = ""
+	str_1 = re.sub('[^ A-Za-z0-9]', '', text.lower())
+    return str_1
+
+def main():
+    string = input()
+    print(clean_string(string))
+
+if __name__ == '__main__':
+    main()
