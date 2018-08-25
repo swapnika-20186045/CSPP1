412ab9f29f7e28e6b3143924bf5cc6279a2d9bfe
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 10:48:50 +0530

    " -> 0 of 3 passed. pylint: -5.56/10 "

--- CSPP1-Practice/M22 (final exam)/assignment2/clean_input.py
+++ CSPP1-Practice/M22 (final exam)/assignment2/clean_input.py
@@ -7,8 +7,8 @@ Date: 25-08-2018
 '''
 
 def clean_string(string):
-	str_1 = ""
-	str_1 = re.sub('[^ A-Za-z0-9]', '', text.lower())
+    str_1 = ""
+    str_1 = re.sub('[^ A-Za-z0-9]', '', text.lower())
     return str_1
 
 def main():
