9f34d877d1c319855862c1222c423a33056ec585
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-25 13:24:28 +0530

    " -> 14 of 26 passed. pylint: 8.33/10 "

--- CSPP1-Practice/M22 (final exam)/Check Sudoku/check_sudoku.py
+++ CSPP1-Practice/M22 (final exam)/Check Sudoku/check_sudoku.py
@@ -81,7 +81,6 @@ def main():
         main function to read input sudoku from console
         call check_sudoku function and print the result to console
     '''
-    
     # initialize empty list
     sudoku = []
     line = "123456789"
